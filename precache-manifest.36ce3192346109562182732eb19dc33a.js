861ed1a9a357d739f113c327c5c03ffaa9fa018f
lee <iop2480@naver.com>
2018-11-21 23:25:06 +0900

    Updates

---
+++ precache-manifest.36ce3192346109562182732eb19dc33a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "753f852e02cea3e170f8",
+    "url": "/movie_app/static/js/main.753f852e.chunk.js"
+  },
+  {
+    "revision": "0bee8e5dcb3c5e319809",
+    "url": "/movie_app/static/js/1.0bee8e5d.chunk.js"
+  },
+  {
+    "revision": "753f852e02cea3e170f8",
+    "url": "/movie_app/static/css/main.bac9e08d.chunk.css"
+  },
+  {
+    "revision": "10b0b3ee3b598f67532253bbc9e8513b",
+    "url": "/movie_app/index.html"
+  }
+];
